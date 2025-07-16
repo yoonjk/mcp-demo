adc25e91397402d8b2409101477f6fde12bb7720
JG Yun <nexweb1@gmail.com>
2025-07-16 22:43:20 +0900

    Modify productService

--- src/main/java/com/example/mcp_demo/domain/Product.java
+++ src/main/java/com/example/mcp_demo/domain/Product.java
@@ -1,5 +1,8 @@
 package com.example.mcp_demo.domain;
 
+import java.math.BigDecimal;
+
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -7,16 +10,34 @@ import lombok.ToString;
 @ToString
 @Getter
 @Setter
+
 public class Product {
+
+	private Long id;
+	
+
     private String productName;
+	
+
     private String description;
+	
+
+    private String catalog;
+	
+	
+
     private long price;
+	
 
-    public Product() {}
+	private BigDecimal rate;
 
-    public Product(String productName, String description, long price) {
-        this.productName = productName;
-        this.description = description;
-        this.price = price;
-    }
+	@Builder
+    public Product(Long id, String productName, String description, String catalog, long price, BigDecimal rate) {
+		this.id = id;
+		this.productName = productName;
+		this.description = description;
+		this.catalog = catalog;
+		this.price = price;
+		this.rate = rate;
+	}
 }
--- src/main/java/com/example/mcp_demo/tool/ProductService.java
+++ src/main/java/com/example/mcp_demo/tool/ProductService.java
@@ -1,22 +1,117 @@
 package com.example.mcp_demo.tool;
 
-import com.example.mcp_demo.domain.Product;
-import org.springframework.ai.tool.annotation.Tool;
-import org.springframework.stereotype.Service;
-
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.stereotype.Service;
+
+import com.example.mcp_demo.domain.Product;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class ProductService {
-    private List<Product> products = new ArrayList<Product>();
-    @Tool(name="getProducts",
+  
+	
+    @SuppressWarnings("unchecked")
+	@Tool(name="getProducts",
             description="Get All products in database")
     public List<Product> getProducts() {;
-        products.add(new Product("아이폰13미니", "아이폰 시리즈입니다.", 2500000));
-        products.add(new Product("아이폰13", "아이폰 시리즈입니다.", 3000000));
+        List<Product> products = new ArrayList<Product>();
 
+        products.add(Product.builder().
+        		id(1L).
+        		productName("아이폰13미니").
+        		description("아이폰 시리즈").
+        		catalog("iphone").
+        		price(10000000)
+        		.build());
+        
+        products.add(Product.builder().
+        		id(1L).
+        		productName("아이폰13").
+        		description("아이폰 시리즈").
+        		catalog("iphone").
+        		price(13000000)
+        		.build());
+        
+        
+        
+        List<Product> prods = httpClient();
+        
+        for(Product product : prods) {
+        	products.add(product);
+        }
+        
         return products;
     }
+    
+    @Tool(name = "findByProductName",
+    		description = "Find a product by productName"
+    		)
+    public Product findByProductName(String productName) {
+    	HttpClient client = HttpClient.newHttpClient();
+    	
+        // Create a HttpRequest
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:8060/product/"+productName))
+                .GET()
+                .build();
+        
+        Product product = null;
+        
+        try {
+        	Gson gson = new Gson();
+        	
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            String jsonStr = response.body();
+            
+        	product = gson.fromJson(jsonStr, Product.class);
+        	
+        }  catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }        
+        
+        return product;
+    }
 
+    public List<Product> httpClient() {
+        // Create an HttpClient instance
+        HttpClient client = HttpClient.newHttpClient();
+        
+        // Create a HttpRequest
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:8060/products"))
+                .GET()
+                .build();
+        
+        List<Product> products = null;
+        
+        try {
+        	Gson gson = new Gson();
+            // Send the request and get the response
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            String jsonStr = response.body();
+            
+            Type listOfMyClassObject = new TypeToken<ArrayList<Product>>() {}.getType();
+            products = gson.fromJson(jsonStr, listOfMyClassObject);
+            
+            // Print the response status code and body
+            log.info("Status code: " + response.statusCode());
+            log.info("Response body: " + response.body());
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }        
+        return products;
+    }
 }
